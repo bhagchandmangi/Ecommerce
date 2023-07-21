78cdacd8897e5cc3516f3b0641f1d0fa1c53b9f5
bhagchandmangi <bhagchandmangi@gmail.com>
2023-07-21 16:58:24 +0500

    Created Category Models and Categories Controller

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        List<Category> categories = new List<Category>
+        {
+
+        new Category{Id = 1, Name = "Samsung", DisplayOrder = 1 },
+        new Category{Id = 2, Name = "Apple", DisplayOrder= 2 },
+        new Category{Id = 3, Name = "Oppo", DisplayOrder = 3 },
+        };
+    }
+}
