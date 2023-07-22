4b8ccea4c707ac5735e7264321126cb0514e3cd5
bhagchandmangi <bhagchandmangi@gmail.com>
2023-07-22 13:17:07 +0500

    Make the list static and new entry in list by postman

--- Ecommerce/Controllers/CategoryController.cs
+++ Ecommerce/Controllers/CategoryController.cs
@@ -9,12 +9,23 @@ namespace Ecommerce.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
-        List<Category> categories = new List<Category>
+        public static List<Category> ListOfcategories = new List<Category>
         {
 
         new Category{Id = 1, Name = "Samsung", DisplayOrder = 1 },
         new Category{Id = 2, Name = "Apple", DisplayOrder= 2 },
         new Category{Id = 3, Name = "Oppo", DisplayOrder = 3 },
         };
+
+        [HttpGet]
+        public IEnumerable<Category> Get()
+        {
+            return ListOfcategories;
+        }
+        [HttpPost]
+        public void Post([FromBody] Category category)
+        {
+            ListOfcategories.Add(category); 
+        }
     }
 }
