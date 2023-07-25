cdbad88b2dc7ba2719db599c0af17437c8033fad
bhagchandmangi <bhagchandmangi@gmail.com>
2023-07-25 12:08:11 +0500

    Model Level Validation

--- Ecommerce/Models/Category.cs
+++ Ecommerce/Models/Category.cs
@@ -1,9 +1,13 @@
-﻿namespace Ecommerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Name conn't be null")]
         public string Name { get; set; }
+        [Required(ErrorMessage ="DisplayOrder is Required")]
         public int DisplayOrder { get; set; }
         //public DateTime DateTime { get; set; } = DateTime.Now();
     }
